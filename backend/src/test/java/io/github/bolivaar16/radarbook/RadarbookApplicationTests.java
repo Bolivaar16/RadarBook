b921e93ff3fa5c3a764c554b136e0c3b706c9e0f
Bolivaar16 <alexbolivarc16@gmail.com>
2026-08-19 16:00:30 +0200

    Feat: add Spring Boot backend

---
+++ backend/src/test/java/io/github/bolivaar16/radarbook/RadarbookApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bolivaar16.radarbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadarbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
