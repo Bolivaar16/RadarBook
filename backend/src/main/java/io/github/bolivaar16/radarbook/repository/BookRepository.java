2e157e9ac6d899d3a7f9628a88e7fee27824914d
Bolivaar16 <alexbolivarc16@gmail.com>
2026-08-20 18:28:05 +0200

    Feature: Creation of Repositories with JpaRepositories.

--- backend/src/main/java/io/github/bolivaar16/radarbook/repository/BookRepository.java
+++ backend/src/main/java/io/github/bolivaar16/radarbook/repository/BookRepository.java
@@ -1,5 +1,9 @@
 package io.github.bolivaar16.radarbook.repository;
 
-public class BookRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.github.bolivaar16.radarbook.model.Book;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
 }
